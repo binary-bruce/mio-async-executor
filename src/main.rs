bc6d1b7f5dea0fdceae09dd246b7f3854e246bd9
binary.bruce <myth.litao@foxmail.com>
2024-05-19 20:43:49 +0800

    example module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use delay::delay;
-use udp_socket::UdpSocket;
-use yield_now::yield_now;
-
 use crate::core::new_executor_spawner;
+use crate::example::{tick, tock, udp_client, udp_server};
 
 mod core;
 mod delay;
+mod example;
 mod executor;
 mod reactor;
 mod spawner;
@@ -27,40 +25,4 @@ fn main() {
 
     println!("executor is running...");
     executor.run();
-}
-
-async fn tick() {
-    loop {
-        println!("tick");
-        yield_now().await;
-    }
-}
-
-async fn tock() {
-    loop {
-        println!("tock");
-        yield_now().await;
-    }
-}
-
-async fn udp_server() {
-    let socket = UdpSocket::bind("127.0.0.1:8080").unwrap();
-
-    let mut buf = [0; 100];
-    let (size, _) = socket.recv_from(&mut buf).await.unwrap();
-
-    println!("server: received data from client - {}", unsafe {
-        std::str::from_utf8_unchecked(&buf[0..size])
-    });
-}
-
-async fn udp_client() {
-    let socket = UdpSocket::bind("127.0.0.1:8001").unwrap();
-    let data = "hello and goodbye".to_owned();
-    let _ = socket
-        .send_to(data.as_bytes(), "127.0.0.1:8080".parse().unwrap())
-        .await;
-    println!("client: sent data to server - {}", data);
-
-    delay(1).await;
 }
