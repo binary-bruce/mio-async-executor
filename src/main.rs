0b7b08b49c8334aa2474a44bc7f59a793a8fe72a
binary.bruce <myth.litao@foxmail.com>
2024-05-19 08:51:24 +0800

    an example of simple udp client and server tasks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use delay::delay;
 use udp_socket::UdpSocket;
 
 use crate::core::new_executor_spawner;
@@ -13,8 +14,8 @@ mod waker;
 
 fn main() {
     let (executor, spawner) = new_executor_spawner();
-    spawner.spawn(async_delay());
-    spawner.spawn(async_main());
+    spawner.spawn(udp_server());
+    spawner.spawn(udp_client());
     // Drop this spawner, so that the `run` method can stop as soon as all other
     // spawners (stored within tasks) are dropped
     drop(spawner);
@@ -23,22 +24,32 @@ fn main() {
     executor.run();
 }
 
-async fn async_main() {
-    let socket = UdpSocket::bind("127.0.0.1:8000").unwrap();
+async fn udp_server() {
+    let socket = UdpSocket::bind("127.0.0.1:8080").unwrap();
 
-    // Receives a single datagram message on the socket. If `buf` is too small to hold
-    // the message, it will be cut off.
-    let mut buf = [0; 10];
-    let (amt, src) = socket.recv_from(&mut buf).await.unwrap();
+    for _ in 1..10 {
+        let mut buf = [0; 10];
+        let (size, _) = socket.recv_from(&mut buf).await.unwrap();
 
-    // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-    let buf = &mut buf[..amt];
-    buf.reverse();
-    socket.send_to(buf, src).await.unwrap();
+        println!("server: received data from client - {}", unsafe {
+            std::str::from_utf8_unchecked(&buf[0..size])
+        });
+    }
+
+    println!("server: bye..");
 }
 
-async fn async_delay() {
-    println!("delay 10 seconds..");
-    delay::delay(10).await;
-    println!("time to work..")
+async fn udp_client() {
+    let socket = UdpSocket::bind("127.0.0.1:8001").unwrap();
+    for i in 1..10 {
+        let data = format!("{}", i);
+        let _ = socket
+            .send_to(data.as_bytes(), "127.0.0.1:8080".parse().unwrap())
+            .await;
+        println!("client: sent data to server - {}", data);
+
+        delay(1).await;
+    }
+
+    println!("client: see you..");
 }
