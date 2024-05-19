2f38e8359e466f3faace2bb18db5e8b8410c9086
binary.bruce <myth.litao@foxmail.com>
2024-05-19 10:34:25 +0800

    use waker to implement yield

--- src/yield_now.rs
+++ src/yield_now.rs
@@ -1,6 +1,39 @@
-use crate::delay;
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{
+        atomic::{AtomicBool, Ordering::Relaxed},
+        Arc,
+    },
+    task::{Context, Poll},
+};
 
-// use delay as implementation, but can be improved by not spawning a thread to sleep
 pub async fn yield_now() {
-    delay(1).await;
+    Yield::new().await;
+}
+
+struct Yield {
+    yielded: Arc<AtomicBool>,
+}
+
+impl Yield {
+    fn new() -> Self {
+        Yield {
+            yielded: Arc::new(AtomicBool::new(false)),
+        }
+    }
+}
+
+impl Future for Yield {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.yielded.load(Relaxed) {
+            Poll::Ready(())
+        } else {
+            self.yielded.swap(true, Relaxed);
+            cx.waker().clone().wake();
+            Poll::Pending
+        }
+    }
 }
