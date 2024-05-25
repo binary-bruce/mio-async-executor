5e4232e4f36600bdf7973e681d2d2b97866c8af8
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-05-25 16:12:35 +0800

    test: Some unit tests (#4)
    * some tests
    
    * format

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -12,3 +12,69 @@ pub fn new_executor_spawner() -> (Executor, Spawner) {
 
     (Executor { ready_queue }, Spawner { task_sender })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc;
+
+    use super::new_executor_spawner;
+
+    #[test]
+    fn the_answer_is_42() {
+        let (executor, spawner) = new_executor_spawner();
+        spawner.spawn(async {
+            assert_eq!(42, 42);
+        });
+        drop(spawner);
+        executor.run();
+    }
+
+    #[test]
+    fn test_async_communicate_by_channel() {
+        let (executor, spawner) = new_executor_spawner();
+
+        // though channel is not for async tasks, this is only for testing purpose
+        let (sender, receiver) = mpsc::channel::<String>();
+        spawner.spawn(async move {
+            let _ = sender.send("hello".to_owned());
+        });
+
+        spawner.spawn(async move {
+            // yeah, it's blocking, should looking for some async alternative
+            let received = receiver.recv().unwrap();
+            assert_eq!("hello", received);
+        });
+
+        drop(spawner);
+        executor.run();
+    }
+
+    #[test]
+    fn test_udp_client_and_server() {
+        let (executor, spawner) = new_executor_spawner();
+        spawner.spawn(udp_server("hello".to_owned()));
+        spawner.spawn(udp_client("hello".to_owned()));
+
+        drop(spawner);
+        executor.run();
+    }
+
+    pub async fn udp_server(expected: String) {
+        use super::futures::udp_socket2::UdpSocket;
+        let socket = UdpSocket::bind("127.0.0.1:8888").unwrap();
+
+        let mut buf = [0; 100];
+        let (size, _) = socket.recv_from(&mut buf).await.unwrap();
+
+        let received = unsafe { std::str::from_utf8_unchecked(&buf[0..size]) };
+        assert_eq!(expected, received);
+    }
+
+    async fn udp_client(greeting: String) {
+        use super::futures::udp_socket::UdpSocket;
+
+        let socket = UdpSocket::bind("127.0.0.1:9999").unwrap();
+        let server_addr = "127.0.0.1:8888".parse().unwrap();
+        let _ = socket.send_to(greeting.as_bytes(), server_addr).await;
+    }
+}
