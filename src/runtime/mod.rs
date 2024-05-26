eea5616db43fa5ab4594cb514c3c8a5352f24d2e
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-05-26 10:53:05 +0800

    feat: block_on (powered by extreme crate) (#5)
    * use extreme lib to implement block_on
    
    * test
    
    * fix clippy

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -15,9 +15,11 @@ pub fn new_executor_spawner() -> (Executor, Spawner) {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::mpsc;
+    use std::{sync::mpsc, thread};
 
-    use super::new_executor_spawner;
+    use super::{core::executor::Executor, futures::delay::delay, new_executor_spawner};
+
+    static GREETING: &str = "hello";
 
     #[test]
     fn the_answer_is_42() {
@@ -36,13 +38,13 @@ mod tests {
         // though channel is not for async tasks, this is only for testing purpose
         let (sender, receiver) = mpsc::channel::<String>();
         spawner.spawn(async move {
-            let _ = sender.send("hello".to_owned());
+            let _ = sender.send(GREETING.to_owned());
         });
 
         spawner.spawn(async move {
             // yeah, it's blocking, should looking for some async alternative
             let received = receiver.recv().unwrap();
-            assert_eq!("hello", received);
+            assert_eq!(GREETING, received);
         });
 
         drop(spawner);
@@ -52,11 +54,28 @@ mod tests {
     #[test]
     fn test_udp_client_and_server() {
         let (executor, spawner) = new_executor_spawner();
-        spawner.spawn(udp_server("hello".to_owned()));
-        spawner.spawn(udp_client("hello".to_owned()));
+        spawner.spawn(udp_server(GREETING.to_owned()));
+        spawner.spawn(udp_client(GREETING.to_owned()));
 
         drop(spawner);
         executor.run();
+    }
+
+    // run `cargo test -- --test-threads 1`
+    #[test]
+    fn block_the_udp_server() {
+        let (executor, spawner) = new_executor_spawner();
+        spawner.spawn(async {
+            delay(1).await; // wait the blocking server to start up
+            udp_client(GREETING.to_owned()).await;
+        });
+
+        drop(spawner);
+        // run the execturo in another thread so that below code can run
+        let handle = thread::spawn(move || executor.run());
+
+        Executor::block_on(udp_server(GREETING.to_owned()));
+        let _ = handle.join();
     }
 
     pub async fn udp_server(expected: String) {
