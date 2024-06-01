2a40ecce14458d1584796245726f44bb75e39e73
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-06-01 11:28:32 +0800

    feat: spawn_block (#10)
    * spawn new thread
    
    * use thread pool
    
    * refactor

--- src/runtime/futures.rs
+++ src/runtime/futures.rs
@@ -1,4 +1,5 @@
 pub mod delay;
+pub mod spawn_blocking;
 pub mod udp_socket;
 pub mod udp_socket2;
 pub mod yield_now;
---
+++ src/runtime/futures/spawn_blocking.rs
@@ -0,0 +1,79 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex, OnceLock},
+    task::{Context, Poll, Waker},
+};
+
+use threadpool::ThreadPool;
+
+pub fn spawn_blocking<T, F>(f: F) -> SpawnBlocking<T>
+where
+    F: FnOnce() -> T,
+    F: Send + 'static,
+    T: Send + 'static,
+{
+    static THREAD_POOL: OnceLock<threadpool::ThreadPool> = OnceLock::new();
+
+    let inner = SpawnBlockingInner::new();
+    THREAD_POOL.get_or_init(|| ThreadPool::new(4)).execute({
+        let inner_cloned = inner.clone();
+        move || {
+            let value = f();
+            let mut inner = inner_cloned.lock().unwrap();
+            inner.value = Some(value);
+            if let Some(waker) = inner.waker.take() {
+                waker.wake()
+            }
+        }
+    });
+
+    SpawnBlocking { inner }
+}
+
+pub struct SpawnBlocking<T> {
+    inner: Arc<Mutex<SpawnBlockingInner<T>>>,
+}
+
+struct SpawnBlockingInner<T> {
+    value: Option<T>,
+    waker: Option<Waker>,
+}
+
+impl<T> SpawnBlockingInner<T> {
+    fn new() -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(SpawnBlockingInner {
+            value: None,
+            waker: None,
+        }))
+    }
+}
+
+impl<T: Send> Future for SpawnBlocking<T> {
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut inner = self.inner.lock().unwrap();
+        if let Some(value) = inner.value.take() {
+            return Poll::Ready(value);
+        }
+
+        inner.waker = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::runtime::core::executor::Executor;
+
+    use super::spawn_blocking;
+
+    #[test]
+    fn test_long_running_computation() {
+        let answer = Executor::block_on(spawn_blocking(|| 42));
+
+        assert_eq!(42, answer);
+    }
+}
