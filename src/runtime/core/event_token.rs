18f9bfc8a5915ef3736bc4bd5fc05252a376d706
binary.bruce <myth.litao@foxmail.com>
2024-08-15 21:41:49 +0800

    refactor

--- src/runtime/core/event_token.rs
+++ src/runtime/core/event_token.rs
@@ -1,13 +1,13 @@
 use mio::Token;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub(crate) struct EventToken;
 
 impl EventToken {
     pub(crate) fn next() -> Token {
-        use std::sync::atomic::{AtomicUsize, Ordering};
-
         static CURRENT_TOKEN: AtomicUsize = AtomicUsize::new(0);
 
-        Token(CURRENT_TOKEN.fetch_add(1, Ordering::Relaxed))
+        let value = CURRENT_TOKEN.fetch_add(1, Ordering::Relaxed);
+        Token(value)
     }
 }
